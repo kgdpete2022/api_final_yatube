eed866a5f38c1334f8739100332fbb26f1292f24
Piotr Shopin <mail4dev2022@gmail.com>
2022-12-17 13:42:49 +0600

    11 failed, 17 passed, 5 errors

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -13,5 +13,7 @@ router.register(r'follow', FollowViewSet, basename='follow')
 
 
 urlpatterns = [
-    path('v1/', include(router.urls))
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
