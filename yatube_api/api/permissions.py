e078a9bc3f82a360ca8fc28832f81052b2e2e58e
Piotr Shopin <mail4dev2022@gmail.com>
2022-12-18 09:40:46 +0600

    6 failed, 22 passed, 5 errors

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -8,4 +8,4 @@ class AuthorOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.author
+        return request.method in permissions.SAFE_METHODS or request.user == obj.author
